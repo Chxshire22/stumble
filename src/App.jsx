772257a95454b906d0092f5cea9aebc0d1889786
Chxshire22 <akuindra22@gmail.com>
2023-12-15 14:34:22 +0800

    rudimentary routing completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import { useNavigate } from 'react-router-dom'
+import { RouterProvider,createBrowserRouter } from 'react-router-dom'
 import './App.css'
+import LoginSignout from './components/LoginSignout'
+import Home from './components/Home'
+import CreatePost from './components/CreatePost'
+import FullPost from './components/FullPost'
+import Profile from './components/Profile'
 
 function App() {
 
-  const navigate = useNavigate()
-
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/welcome",
+    element: <LoginSignout />,
+  },
+  {
+    path: "/create-post",
+    element: <CreatePost />,
+  },
+  {
+    path: "/post",
+    //will need the unique ID of post
+    element: <FullPost />,
+  },
+  {
+    path: "/profile",
+    //will need the unique ID of profile
+    element: <Profile />,
+  },
+]);
   return (
     <>
-    
+    <RouterProvider router={router}/>
     </>
   )
 }
