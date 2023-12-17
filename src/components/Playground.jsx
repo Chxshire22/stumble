1c1c6c74a0aa1cd994d3cab571738bd0b0fb9eaf
Chxshire22 <akuindra22@gmail.com>
2023-12-17 18:32:15 +0800

    completed card element for posts in feed

--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -40,12 +40,48 @@ function Playground() {
           alt="hot air balloons"
         />
       </div>
-      <div className="post-details comments">
-        <p className="comment-username">@chxshire22</p>
-        <p className="comment-content">
-          wow can't wait to go there end of this month!
-        </p>
-      </div>
+      <Container className="post-interactions">
+        <Row>
+          <Col>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-chat-fill chat-icon"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9.06 9.06 0 0 0 8 15" />
+            </svg>
+          </Col>
+          <Col xs={7} className="post-details comments">
+            <p className="comment-username">@chxshire22</p>
+            <p className="comment-content">
+              wow can't wait to go there end of this month!
+            </p>
+          </Col>
+          <Col>
+              <div className="likes">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-heart-fill heart-like"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
+                  />
+                </svg>
+                            <p className="likes-count">
+                102 likes
+                            </p>
+              </div>
+          </Col>
+        </Row>
+      </Container>
     </article>
   );
 }
