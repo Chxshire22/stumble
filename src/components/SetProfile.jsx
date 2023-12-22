0adc9db60bd548dcfea89cdd3cf7ff37e47cc9e4
Chxshire22 <akuindra22@gmail.com>
2023-12-22 12:40:24 +0800

    updated SetProfile skeleton

--- src/components/SetProfile.jsx
+++ src/components/SetProfile.jsx
@@ -1,5 +1,12 @@
 export default function SetProfile() {
 	return (
-		<h1>Set your profile here</h1>
+		<>
+		<div className="pfp-container"></div>
+		<form action="">
+		<input type="file" name="" id="" className="upload-pfp"/>
+		<input className="username" type="text" name="" id=""/>
+		<textarea className="user-bio" name="" id="" cols="30" rows="2"></textarea>
+		</form>
+		</>
 	)
 }
