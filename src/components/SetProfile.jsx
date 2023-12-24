c55dab039355014c5c987d33efc8f36a18e99842
Chxshire22 <akuindra22@gmail.com>
2023-12-24 20:35:00 +0800

    fixed button css to not repeat code

--- src/components/SetProfile.jsx
+++ src/components/SetProfile.jsx
@@ -1,12 +1,13 @@
 export default function SetProfile() {
 	return (
-		<>
-		<div className="pfp-container"></div>
-		<form action="">
+		<div className="flex-center-col">
+		<div className="pfp-container set-profile-picture"></div>
+		<form className="flex-center-col" action="">
 		<input type="file" name="" id="" className="upload-pfp"/>
 		<input className="username" type="text" name="" id=""/>
 		<textarea className="user-bio" name="" id="" cols="30" rows="2"></textarea>
 		</form>
-		</>
+		<button className="btn-base">Next</button>
+		</div>
 	)
 }
