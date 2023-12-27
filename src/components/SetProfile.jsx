4ba98b16b0ca3897115b182e7a317ffb2538e5fb
Chxshire22 <akuindra22@gmail.com>
2023-12-27 18:06:36 +0800

    updated fonts - it wasnt working with downloaded fonts

--- src/components/SetProfile.jsx
+++ src/components/SetProfile.jsx
@@ -61,6 +61,7 @@ export default function SetProfile(props) {
         console.log("loading user");
       }
     });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const saveProfile = (e) => {
