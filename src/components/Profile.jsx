ad064cd278cac444d7a0523551db2e4271de6140
Chxshire22 <akuindra22@gmail.com>
2023-12-29 21:30:24 +0800

    buttons and image deco for profile page added

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,67 +1,78 @@
 import { useEffect, useState } from "react";
 import { Image } from "react-bootstrap";
 import { userRef } from "./firebase";
-import { get, child, } from "firebase/database";
+import { get, child } from "firebase/database";
 import { useNavigate, useParams } from "react-router-dom";
+import FeedPostCard from "./FeedPostCard"
 
 function Profile() {
-  // user can edit posts? if uid == auth.currentUser.uid
-  const [profile, setProfile] = useState({})
+	// user can edit posts? if uid == auth.currentUser.uid
+	const [profile, setProfile] = useState({});
 
-  const {uid} = useParams();
+	const { uid } = useParams();
 
-  useEffect(()=>{
-    if(uid){
-      console.log(uid)
-    }
-  },[uid])
+	useEffect(() => {
+		if (uid) {
+			console.log(uid);
+		}
+	}, [uid]);
 
-  useEffect(()=>{
-    get(child(userRef, `/${uid}`))
-    .then((snapshot) => {
-      if (snapshot.exists()) {
-        console.log(snapshot.val());
-        setProfile(snapshot.val())
-      } else {
-        console.log("No data available");
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-  },[uid])
+	useEffect(() => {
+		get(child(userRef, `/${uid}`))
+			.then((snapshot) => {
+				if (snapshot.exists()) {
+					console.log(snapshot.val());
+					setProfile(snapshot.val());
+				} else {
+					console.log("No data available");
+				}
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}, [uid]);
 
-  const navigate = useNavigate()
+	const navigate = useNavigate();
 
-
-  return (
-    <div className="block">
-      <header className="profile-page-header">
-        {/* decided to upload the image to firebase */}
-        <img
-          src="https://firebasestorage.googleapis.com/v0/b/stumble-a6ed0.appspot.com/o/assets%2Fstumble-logo.webp?alt=media&token=72a22d6a-4de1-4a2c-b0bc-08fe0f660c8f"
-          className="stumble-logo"
-          alt=""
-          onClick={() =>navigate("/")}
-        />
-        <Image
-          className="pfp pfp-profile-page"
-          src={profile.displayPic}
-          roundedCircle
-          fluid
-        />
-        <h1 className="profile-page-user blue">{profile.username}</h1>
-        <p className="bio profile-page-bio blue">{profile.bio}</p>
-      </header>
-    </div>
-  );
+	return (
+		<div className="block flex-center-col">
+			<header className="profile-page-header">
+				{/* decided to upload the image to firebase */}
+				<img
+					src="https://firebasestorage.googleapis.com/v0/b/stumble-a6ed0.appspot.com/o/assets%2Fstumble-logo.webp?alt=media&token=72a22d6a-4de1-4a2c-b0bc-08fe0f660c8f"
+					className="stumble-logo"
+					alt=""
+					onClick={() => navigate("/")}
+				/>
+				<Image
+					className="pfp pfp-profile-page"
+					src={profile.displayPic}
+					roundedCircle
+					fluid
+				/>
+				<h1 className="profile-page-user blue">{profile.username}</h1>
+				<p className="bio profile-page-bio blue">{profile.bio}</p>
+			</header>
+			<hr />
+			<div className="profile-body app-body">
+				<div className="feed-btns-col">
+					<button className="btn-base feed-btn">Saved Posts</button>
+					<button className="btn-base feed-btn">Filters</button>
+				</div>
+				<div className="profile-feed feed flex-center-col">
+				<FeedPostCard/>
+				<FeedPostCard/>
+				<FeedPostCard/>
+				<FeedPostCard/>
+				<FeedPostCard/>
+				</div>
+				<img
+					className="feed-image-deco"
+					src="https://firebasestorage.googleapis.com/v0/b/stumble-a6ed0.appspot.com/o/assets%2Fbermuda-79.png?alt=media&token=e4cc1f34-a389-4e38-9232-0f4b53c0d8f0"
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default Profile;
-
-        // <Image
-        //   className="pfp pfp-profile-page"
-        //   src={profile.displayPic}
-        //   roundedCircle
-        //   fluid
-        // />;
