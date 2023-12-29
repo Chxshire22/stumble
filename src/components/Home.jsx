8c41a69ff1f14c87de7e17d46b755df16e4c9aea
Chxshire22 <akuindra22@gmail.com>
2023-12-30 00:58:39 +0800

    added some layout changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,12 @@
 import ModalCreatePost from "./ModalCreatePost";
 import FeedPostCard from "./FeedPostCard";
 import { useState, useEffect } from "react";
-import { db } from "./firebase";
+import { auth, db } from "./firebase";
 import { ref as databaseRef, onValue } from "firebase/database";
-import Button from "react-bootstrap/Button";
+import { signOut } from "firebase/auth";
+// import Button from "react-bootstrap/Button";
+import { Image, Dropdown } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const POSTS_FOLDER_NAME = "posts";
 
@@ -20,34 +23,84 @@ function Home() {
     });
   }, []);
 
-  return (
-    <div>
-      <h1>home page</h1>
-      <div className="button-container">
-        {/* <Button className="open-modal-button" onClick={handleClickOpen}>
-          What&apos;s on your mind?
-        </Button>
-        <ModalCreatePost openModal={openModal} handleClose={handleClose} /> */}
-        <Button variant="primary" onClick={() => setModalShow(true)}>
-          What&apos;s on your mind?
-        </Button>
+  const logout = async () => {
+    try {
+      await signOut(auth);
+      navigate("/welcome");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-        <ModalCreatePost show={modalShow} onHide={() => setModalShow(false)} />
-      </div>
-      <div>
-        {posts.map((post, index) => (
-          <FeedPostCard
-            key={index}
-            username={post.username}
-            location={post.location}
-            text={post.text}
-            date={post.date}
-            image={post.imageLink}
+  const navigate = useNavigate()
+
+  return (
+    <div className="block flex-center-col">
+      <header className="flex-center-row">
+        {/* decided to upload the image to firebase */}
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/stumble-a6ed0.appspot.com/o/assets%2Fstumble-logo.webp?alt=media&token=72a22d6a-4de1-4a2c-b0bc-08fe0f660c8f"
+          className="stumble-logo"
+          alt=""
+          onClick={() => navigate("/")}
+        />
+        <Image
+          className="pfp pfp-profile-page"
+          src={auth.currentUser.photoURL}
+          roundedCircle
+          fluid
+        />
+      </header>
+      <div className="dropdown-container"></div>
+      <hr />
+      <div className="profile-body app-body">
+        <div className="feed-btns-col">
+          <button className="btn-base feed-btn">Filters</button>
+          <Dropdown>
+            <Dropdown.Toggle className="btn-base feed-btn" id="dropdown-basic">
+              Settings
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={logout}>Sign Out</Dropdown.Item>
+              <Dropdown.Item onClick={() => navigate("/set-profile")}>
+                Edit Profile
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+                Toggle Theme (dark mode maybe?)
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        <div className="home-content">
+          <button className="btn-base btn-create-post" onClick={() => setModalShow(true)}>
+              <img className="pfp-badge" src={auth.currentUser.photoURL}  />
+            <p>What&apos;s on your mind?</p>
+          </button>
+          <ModalCreatePost
+            show={modalShow}
+            onHide={() => setModalShow(false)}
           />
-        ))}
+          <div className="profile-feed feed flex-center-col">
+            {posts.map((post, index) => (
+              <FeedPostCard
+                key={index}
+                username={post.username}
+                location={post.location}
+                text={post.text}
+                date={post.date}
+                image={post.imageLink}
+              />
+            ))}
+          </div>
+        </div>
+        <img
+          className="feed-image-deco"
+          src="https://firebasestorage.googleapis.com/v0/b/stumble-a6ed0.appspot.com/o/assets%2Fflamenco-excited-for-a-flight.webp?alt=media&token=56ea81fb-9bf9-48bd-bfd5-ea3413d800bb"
+        />
       </div>
     </div>
-  );
-}
+  );}
 
 export default Home;
+
